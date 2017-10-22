0ca8824b83a90520b0736d4ce7715e266c1ae4b4
Jonas Larsen <jonas.larsen@norsk-tipping.no>
2017-10-22 20:00:47 +0200

    test

--- hello.go
+++ hello.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
+	a := 0
 	fmt.Printf("Hello, world\n")
 	fmt.Printf("Hei, alle sammen!\n")
 	fmt.Printf("Dette er en test!\n")
 	fmt.Printf("Dette er en test2\n")
 	fmt.Print("Dette er en test3\n")
-	fmt.Print("Hei Mina!\n")
+
+	for a < 10 {
+		fmt.Print("Hei Mina!\n")
+		a = a + 1
+	}
 }
