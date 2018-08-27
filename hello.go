1d5657faecb34e3fb62e7912a1ef1571bfaf7311
Jonas Larsen <nfoscoo@gmail.com>
2018-08-27 21:29:17 +0200

    .

--- hello.go
+++ hello.go
@@ -1,18 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jonaslar/stringutil"
+)
 
 func main() {
-
-	a := 0
-	fmt.Printf("Hello, world\n")
-	fmt.Printf("Hei, alle sammen!\n")
-	fmt.Printf("Dette er en test!\n")
-	fmt.Printf("Dette er en test2\n")
-	fmt.Print("Dette er en test3\n")
-
-	for a < 10 {
-		fmt.Print("Hola, mi Amorcito!\n")
-		a = a + 1
-	}
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
